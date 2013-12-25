bd17b30b6b457634c988ee5d8d4919f32c91cab6
Ken Yasue <ken.yasue@clover-studio.com>
2013-12-25 17:27:03 +0100

    change md5 logic

--- src/com/cloverstudio/spikademo/management/FileManagement.java
+++ src/com/cloverstudio/spikademo/management/FileManagement.java
@@ -143,15 +143,20 @@ public class FileManagement {
 
 	public static String md5(String s) {
 		if (s.equals("")) return "";
-		MessageDigest digest;
-		try {
-			digest = MessageDigest.getInstance("MD5");
-			digest.update(s.getBytes(), 0, s.length());
-			String hash = new BigInteger(1, digest.digest()).toString(16);
-			return hash;
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		return "";
+	    MessageDigest digest;
+	    try {
+	        digest = MessageDigest.getInstance("MD5");
+	        digest.reset();
+	        digest.update(s.getBytes());
+	        byte[] a = digest.digest();
+	        int len = a.length;
+	        StringBuilder sb = new StringBuilder(len << 1);
+	        for (int i = 0; i < len; i++) {
+	            sb.append(Character.forDigit((a[i] & 0xf0) >> 4, 16));
+	            sb.append(Character.forDigit(a[i] & 0x0f, 16));
+	        }
+	        return sb.toString();
+	    } catch (NoSuchAlgorithmException e) { e.printStackTrace(); }
+	    return "";
 	}
 }
